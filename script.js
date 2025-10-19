3332b5a4e803873f537d74da967c2b5a8d27c7fc
ProjectDG <87559063+ProjectDG@users.noreply.github.com>
2025-10-19 12:21:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,14 +7,25 @@ fetch('data.json')
   return response.json();
 })
 .then(data => {
+
+    // console.log(data[0].sections)
+    let fullSectionInfo = data[0].sections;
   
       $(document).ready(function() {
 
-        // Title div
-        d3.select("body").append("div").attr("id", "titleDiv").attr("class", "setup");
+        // Nav div
+        d3.select("body").append("div").attr("id", "navDiv").attr("class", "setup");
 
         // Menu Button
-        d3.select("#titleDiv").append("button").attr("class", "buttons").attr("id", "menuButton").text("Sections");
+        d3.select("#navDiv").append("button").attr("class", "buttons").attr("id", "menuButton").text("Sections");
+
+        // Title div
+        d3.select("#navDiv").append("div").attr("id", "titleDiv").attr("class", "setup");
+
+        // Title
+        let title = fullSectionInfo["section"];
+        console.log(title);
+        d3.select("#titleDiv").append("h1").attr("id", "Title").text(title);
 
         // Main div
         d3.select("body").append("div").attr("id", "mainContainer").attr("class", "setup");
@@ -26,7 +37,10 @@ fetch('data.json')
 
         // Sections Menu Click Function
         $('body').on('click', '#menuButton', function(event) {
+            $("#titleDiv").empty();
             $("#mainContainer").empty();
+
+            d3.select("#mainContainer").append("button").attr("class", "buttons section-buttons").attr("id", "placeholder").text(title);   // --------------------------  fix id
         });    
 
 
