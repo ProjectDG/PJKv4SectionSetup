122ca13676ee61d9521216c8f045eda0283825ae
ProjectDG <87559063+ProjectDG@users.noreply.github.com>
2025-10-19 23:21:56 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -14,25 +14,33 @@ fetch('data.json')
       $(document).ready(function() {
 
         // Nav div
-        d3.select("body").append("div").attr("id", "navDiv").attr("class", "setup");
-
-        // Menu Button
-        d3.select("#navDiv").append("button").attr("class", "buttons").attr("id", "menuButton").text("Sections");
+        d3.select("body").append("div").attr("id", "navDiv").attr("class", "setup nav-divs");
 
         // Title div
         d3.select("#navDiv").append("div").attr("id", "titleDiv").attr("class", "setup");
 
-        // Title
-        let title = fullSectionInfo["section"];
-        console.log(title);
-        d3.select("#titleDiv").append("h1").attr("id", "Title").text(title);
 
         // Main div
         d3.select("body").append("div").attr("id", "mainContainer").attr("class", "setup");
 
+        // Bottom div
+        d3.select("body").append("div").attr("id", "bottomDiv").attr("class", "setup nav-divs");
 
-        // Image Test
-        d3.select("#mainContainer").append("img").attr("src", "./images/liquorOverstock.png").attr("alt", "Liquor Overstock");
+        // Menu Button
+        d3.select("#bottomDiv").append("button").attr("class", "buttons").attr("id", "menuButton").text("Sections");
+
+
+
+
+
+
+
+
+
+
+        // Initial Image & Title
+        d3.select("#titleDiv").append("h1").attr("id", "Title").text("PJK Neighborhood Chinese");
+        d3.select("#mainContainer").append("img").attr("src", "./images/fullBarComplete.png").attr("alt", "PJK Neighborhood Chinese");
 
 
         // Sections Menu Click Function
@@ -41,7 +49,21 @@ fetch('data.json')
             $("#mainContainer").empty();
 
             d3.select("#mainContainer").append("button").attr("class", "buttons section-buttons").attr("id", "placeholder").text(title);   // --------------------------  fix id
-        });    
+
+
+              
+          });    
+
+
+
+
+          // Title                                                                          use for different button click
+          // let title = fullSectionInfo["section"];
+          // console.log(title);
+          // d3.select("#titleDiv").append("h1").attr("id", "Title").text(title);
+
+
+
 
 
 
